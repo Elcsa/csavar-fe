13026208f93316217d103099676b7e1d9cbac7b3
Elcsa <81393593+Elcsa@users.noreply.github.com>
2023-01-31 19:36:52 +0100

    kesz

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { idText } from 'typescript';
 import { ftruncateSync } from 'fs';
-import Popup from 'reactjs-popup';
-
+import { throws } from 'assert';
 
 interface State{
   postTipus:string,
@@ -13,6 +11,11 @@ interface State{
   postHosz:number,
   postAr:number,
   csavarok:Csavar[]
+  show:boolean;
+  selectedItem:Csavar
+  rendelesDarab:number;
+  rendelesDarabmax:number;
+  rendelesDarabMin:number
 }
 interface Csavar{
   id:number,
@@ -32,9 +35,16 @@ class App extends React.Component<{},State>{
       postKeszlet:0,
       postHosz:0,
       postAr:0,
-      csavarok:[]
+      csavarok:[],
+      show: false,
+      rendelesDarab:0,
+      rendelesDarabmax:0,
+      rendelesDarabMin:0,
+
+      selectedItem:{id:-1,tipus:"",hosz:0,keszlet:0,ar:0}
     }
   }
+  toggle=()=>this.setState((currentState)=>({show:!currentState.show}));
   addtoDB=async()=>{
     const{postTipus,postKeszlet,postHosz,postAr}=this.state
     let csavar={
@@ -43,7 +53,7 @@ class App extends React.Component<{},State>{
       "keszlet":postKeszlet,
       "ar":postAr
     };
-    let response=await fetch("http://localhost:3000/csavar",{
+    let response = await fetch("http://localhost:3000/csavar",{
       method:"POST",
       headers:{
         "Content-Type":"application/json"
@@ -68,15 +78,88 @@ class App extends React.Component<{},State>{
       })
       this.loadData()
     }
-    popUprendeles=async(id:number)=>{
-
+    handleClick=(item:Csavar)=>{
+      this.setState({
+        selectedItem:item,
+        show:true
+      })
     }
+    resetDisplay=()=>{
+      this.setState({
+        rendelesDarab:0,
+        show:false,
+        selectedItem:{id:-1,tipus:"",hosz:0,keszlet:0,ar:0}
+      })
+    }
+    megrendel=async(id:number)=>{
+      let rendeltdarabszam={
+        "db":this.state.rendelesDarab
+      }
+      let url="http://localhost:3000csavar/"+id+"/rendeles"
+      let response=await fetch(url,{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(rendeltdarabszam)
+      })
+      this.resetDisplay()
+      this.loadData()
+    }
+
     render(){
       const{postTipus,postKeszlet,postHosz,postAr}=this.state
-      const PopupExample=()=>(<Popup trigger={<button>trigger</button>}position="right center">
-        <div>Popup content here</div>
-      </Popup>)
+      
       return<div>
+        <div className='container'>
+          <div className='row justify-content-center'>
+          <div className="col-sm-4 text-center">
+            csavar tipusa:<br/><input type="text" value={postTipus} onChange={e=>this.setState({postTipus:e.currentTarget.value})}/>
+
+          </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className="col-sm-4 text-center">
+              csavar hosza mm:<br/><input type="number" min={0} value={postHosz} onChange={e=>this.setState({postHosz:parseInt(e.currentTarget.value)})}/>
+
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className="col-sm-4 text-center">
+              keszlet (db):<br/><input type="number" min={0} value={postKeszlet} onChange={e=>this.setState({postKeszlet:parseInt(e.currentTarget.value)})}/>
+
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className="col-sm-4 text-center">
+              ar(ft):<br/><input type="number" min={1} value={postAr} onChange={e=>this.setState({postAr:parseFloat(e.currentTarget.value)})}/>
+
+            </div>
+          </div>
+          <div className='row justify-content-center mt-3'>
+          <div className="col text-center">
+            <button className='btn btn-secondary' onClick={this.addtoDB}>felvesz</button><br/>
+
+          </div>
+          </div>
+          <div className='row justify-content-center mt-3'>
+            <div className="col text-center">
+              <p className='text-danger'>{}</p>
+            </div>
+          </div>
+        </div>
+        {this.state.show && <div className='row justify-content-center pt-3 pb-3'>
+        <div className="col text-center">
+          <h2>biztoss megrendeled ezt a csavart? <strong>{this.state.selectedItem.tipus}</strong></h2>
+          <input type="number" name="db" value={this.state.rendelesDarab} onChange={e =>this.setState({rendelesDarab:parseInt(e.currentTarget.value)})}/>db<br/>
+          <button onClick={()=>this.megrendel(this.state.selectedItem.id)}className='btn btn-success mt-2'>Megrendel</button>
+          <br/>
+          <button onClick={this.toggle} className='btn btn-danger mt-2'>megse</button>
+          </div>
+        </div>}
+
+
+
         <div className='row'>
           {this.state.csavarok.map(item=>(
             <div className='col-md-4'>
@@ -89,6 +172,7 @@ class App extends React.Component<{},State>{
                 </div>
                 <div className='card-footer'>
                   <button onClick={(event)=>this.deleteFromData(item.id)}>torles</button>
+                  <button onClick={(event)=> this.handleClick(item)}>rendel</button>
                 </div>
               </div>
             </div>
@@ -97,5 +181,5 @@ class App extends React.Component<{},State>{
       </div>
     }
   }
-}
+
 export default App;
